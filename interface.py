eeed9792d7f74c13de54704fd36e34b9fe74c732
Masuda <suzuka4180@gmail.com>
2026-04-16 21:20:04 +0900

    night

--- interface.py
+++ interface.py
@@ -21,25 +21,31 @@ class Interface():
 
         return self.coeff * val
     
-    def F(self, x, y):
-        VSMALL = 1e-13
-        rr = tf.sqrt(x*x + y*y )
-        theta = tf.math.acos(y / (rr + VSMALL))
-        R_0 = 1.0
-        F = rr - R_0
+    def F(self, x, y, t):
+        eps= 1e-13
+        rr = tf.sqrt(x*x + y*y + eps)
+        r = tf.sqrt(rr)
+        
+        theta = tf.math.acos(y / (rr + eps))
+        
+        P2 = 3.0 * (y**2 / r) - 1.0
+        a2 = 0.4
+        Rmax_theta = 1.0 + a2 * P2 * tf.cos(t)
+        
+        F = rr - Rmax_theta
 
         return F
     #########################
     
     def P_jet(self, theta):
         p_jet = self.coeff_Pj * tf.math.cos(np.pi - theta)
-
+        p_jet=0
         return p_jet
     
     ##################################
     def Tau_jet(self, theta):
         tau_jet = -self.coeff_Tj * tf.math.sin(theta)
-
+        tau_jet=0
         return tau_jet
 
     
@@ -50,13 +56,16 @@ class Interface():
             tape.watch(y)
             tape.watch(t)
             F = self.F(x, y, t)
+            
         Fx = tape.gradient(F, x)
         Fy = tape.gradient(F, y)
 
         denom = tf.math.sqrt(Fx**2 + Fy**2)
         Fx /= denom
         Fy /= denom
-
+        
+        del tape
+        
         return Fx, Fy
     
     ##########################
@@ -64,11 +73,13 @@ class Interface():
         with tf.GradientTape(persistent=True) as tape:
             tape.watch(x)
             tape.watch(y)
-            tape.watch(t)
             nx, ny = self.normal(x, y, t)
+            
         nxx = tape.gradient(nx, x)
         nyy = tape.gradient(ny, y)
-
+        
+        del tape
+    
         kappa = nxx + nyy
 
         return kappa
